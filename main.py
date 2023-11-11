84f838a3f9c5a11edf0bc7aba018f88734bd37d7
kartheek <50788154+kyk-001@users.noreply.github.com>
2023-11-11 01:23:36 +0000

    Dev code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import apikey
+import openai
+openai.api_key = apikey.api
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
+documents = SimpleDirectoryReader("./data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("Hi")
+print(response)
